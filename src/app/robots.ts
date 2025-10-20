fd2900970f00794de3533df5138474a33ca64b46
MisterPingouin <anthony.axeldisplay@gmail.com>
2025-10-20 09:36:49 +0200

    fix robot

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,12 +2,18 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   const host = "https://olmebar.com";
+
   return {
     rules: [
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/api/", "/draft", "/draft/*", "/preview", "/_next/static/chunks/*.map"],
+        disallow: [
+          "/api/",
+          "/draft/",
+          "/preview/",
+          "/_next/static/chunks/*.map",
+        ],
       },
     ],
     sitemap: `${host}/sitemap.xml`,
