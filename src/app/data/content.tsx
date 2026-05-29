bab5042cef59171977fe1dd9418a967a5ca878f2
MisterPingouin <anthony.axeldisplay@gmail.com>
2026-05-29 14:52:22 +0200

    update content

--- src/app/data/content.tsx
+++ src/app/data/content.tsx
@@ -181,7 +181,7 @@ export const MIXO_SECTIONS: CocktailSection[] = [
             {
         name: "Pedro y Palomino",
         notes:
-          "Tannique, pétillant — Oloroso, eau-de-vie de mirabelle, super vin & ginger ale.",
+          "Tannique, pétillant —  Xérès oloroso, mirabelle, super vin & ginger ale.",
         price: 11,
       },
                   {
