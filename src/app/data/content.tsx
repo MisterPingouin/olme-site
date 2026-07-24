d7920f9759c119ef6182b7faa88583f7ace1514c
MisterPingouin <anthony.axeldisplay@gmail.com>
2026-07-24 17:40:42 +0200

    update content

--- src/app/data/content.tsx
+++ src/app/data/content.tsx
@@ -162,7 +162,7 @@ export const MIXO_SECTIONS: CocktailSection[] = [
       {
         name: "Été perdu",
         notes:
-          "Frais, exotique — Rhum ambré, Rooibos glacé, fruit de la passion, fêve de tonka & citron.",
+          "Frais, exotique — Rhum pineapple, Rooibos glacé, fruit de la passion, fêve de tonka & citron.",
         price: 9,
         happyHourPrice: 8,
 
@@ -588,7 +588,7 @@ export const BRUT_FOOD: { title: string; items: { name: string; price: number }[
     { name: "Saucisse laotienne à la citronnelle", price: 9 },
     // { name: "Brillat savarin & noix", price: 8 },
     // { name: "Tomme de brebis basque", price: 8 },
-    { name: "Burrata des Pouilles, pesto végétal aux graines de tournesol", price: 9.5 },
+    // { name: "Burrata des Pouilles, pesto végétal aux graines de tournesol", price: 9.5 },
     { name: "Sprats légèrement fumés ( cousin de la sardine )", price: 9 },
     // { name: "Terrine de campagne & pickles", price: 9 },
     { name: "Saucisson de bellota", price: 10 },
