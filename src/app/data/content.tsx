1bb4ab035c6da3d658eb4be7aa96c60e51d6360f
MisterPingouin <anthony.axeldisplay@gmail.com>
2026-08-08 17:33:42 +0200

    update content

--- src/app/data/content.tsx
+++ src/app/data/content.tsx
@@ -586,12 +586,12 @@ export const WINES: WineSection[] = [
 export const BRUT_FOOD: { title: string; items: { name: string; price: number }[] } = {
   title: "Brut Food",
   items: [
-    { name: "Olives de Kalamata & d’Italie", price: 5 },
+    // { name: "Olives de Kalamata & d’Italie", price: 5 },
     { name: "Artichauts alla Romana", price: 6 },
     { name: "Salade de courgette coréenne (oi muchim)", price: 6 },
     { name: "Houmous de petits pois et amandes, radis multicolores", price: 8 },
     // { name: "Croquettes de choux-fleurs au paprika, crème sweet & sour", price: 9 },
-    { name: "Croquettes de scamorza, tomates anciennes, harissa maison", price: 12 },
+    // { name: "Croquettes de scamorza, tomates anciennes, harissa maison", price: 12 },
     { name: "Saucisse laotienne à la citronnelle", price: 9.5 },
     // { name: "Brillat savarin & noix", price: 8 },
     // { name: "Tomme de brebis basque", price: 8 },
