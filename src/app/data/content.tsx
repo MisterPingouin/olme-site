fda52b2989699e26f9efc6037cc147414ded0afe
MisterPingouin <anthony.axeldisplay@gmail.com>
2026-09-23 19:34:38 +0200

    update content

--- src/app/data/content.tsx
+++ src/app/data/content.tsx
@@ -57,11 +57,11 @@ export const SOFTS: SoftItem[] = [
   // },
   // { name: "Ninkasi - Ginger Ale Bio", price: 5, sizeCl: 25 },
     { name: "Ninkasi Tonic", price: 5, sizeCl: 25 },
-//     { 
-//   name: "ChariTea - Mate Bio",
-//   price: 5,
-//   sizeCl: 33,
-// },
+    { 
+  name: "ChariTea - Mate Bio",
+  price: 5,
+  sizeCl: 33,
+},
   {
     name: "Uma - Orangeade Bio",
     price: 6,
